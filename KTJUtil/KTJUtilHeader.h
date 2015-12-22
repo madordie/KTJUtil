0afcfc6ee12e1a0a5c68e6953df85a93b09eac95
sunjigang <sunjigang@fangdd.com>
2015-12-22 12:08:41 +0800

    增加POD引用文件

---
+++ KTJUtil/KTJUtilHeader.h
@@ -0,0 +1,23 @@
+//
+//  KTJUtilHeader.h
+//  Demp_KTJUtil
+//
+//  Created by 孙继刚 on 15/12/22.
+//  Copyright © 2015年 Madordie. All rights reserved.
+//
+
+#ifndef KTJUtilHeader_h
+#define KTJUtilHeader_h
+
+#import "NSObject+KTJUtil.h"
+#import "NSString+KTJUtil.h"
+
+#import "UIView+KTJUtil.h"
+#import "UIImage+KTJUtil.h"
+#import "UIControl+KTJUtil.h"
+#import "UIGestureRecognizer+KTJUtil.h"
+#import "UITableView+KTJUtil.h"
+#import "UITableViewCell+KTJUtil.h"
+#import "UITableViewHeaderFooterView+KTJUtil.h"
+
+#endif /* KTJUtilHeader_h */
